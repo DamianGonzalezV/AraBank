d9a22b5c76087e6316a93112cae77970b5e9e582
damian.velazquez@uadec.edu.mx <damianvelazquez@uadec.edu.mx>
2025-08-29 11:22:16 -0600

    Add log in page

--- public/script.js
+++ public/script.js
@@ -6,11 +6,27 @@ const signUpBtnLandpage = document.querySelector(".sign-up-btn-landpage");
 // Sign up form
 const signUpBtn = document.querySelector(".sign-up-btn");
 const signUpForm = document.querySelector("#sign-up-form");
+const loginForm = document.querySelector("#log-in-form");
+const toogleFormLine = document.querySelector(".log-in-toogle-line");
+const toogleFormBtn = document.querySelector(".log-in-toogle-btn");
 
 // Sign up inputs
 const nameInput = document.querySelector("#name-input");
 const usernameInput = document.querySelector("#username-input");
 const emailInput = document.querySelector("#email-input");
+
+// Variables
+let toogleAtLoginStatus = false;
+
+toogleFormBtn.addEventListener("click", () => {
+  toogleAtLoginStatus = !toogleAtLoginStatus;
+  signUpForm.classList.toggle("hide");
+  loginForm.classList.toggle("hide");
+
+  toogleFormBtn.textContent = toogleAtLoginStatus
+    ? "Change to sign up here"
+    : "Change to log in here";
+});
 
 // Send form with async promise
 signUpBtn.addEventListener("click", (e) => {
