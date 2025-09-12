c1ee394a8c992c30d013cbc8c71c93c4b37583bf
damian.velazquez@uadec.edu.mx <damianvelazquez@uadec.edu.mx>
2025-09-12 07:39:44 -0600

    display welcome message

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,9 @@
 "use strict";
 
-const welcomeUser = document.querySelector(".welcome-row-user-span");
+let welcomeUser = document.querySelector(".welcome-row-user-span");
 
-const storedUsername = localStorage.getItem("activeUser");
+let storedUsername = localStorage.getItem("activeUser");
 
-welcomeUser.textContent = storedUsername ? storedUsername : "";
+(function setWelcomeMessage() {
+  welcomeUser.textContent = storedUsername ? storedUsername : "";
+})();
