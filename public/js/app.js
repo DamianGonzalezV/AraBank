45a110fd3abd58b556f9c90f6b7280bbebed6898
damian.velazquez@uadec.edu.mx <damianvelazquez@uadec.edu.mx>
2025-09-20 02:19:30 -0600

    Display request and send div

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,28 @@
 "use strict";
 
 let welcomeUser = document.querySelector(".welcome-row-user-span");
-
 let storedUsername = localStorage.getItem("activeUser");
+
+let requestBtn = document.querySelector(".account-balance-request-button");
+let sendBtn = document.querySelector(".account-balance-send-button");
+let requestDiv = document.querySelector(".account-balance-request");
+let sendDiv = document.querySelector(".account-balance-send");
+
+// Display username
 
 (function setWelcomeMessage() {
   welcomeUser.textContent = storedUsername ? storedUsername : "";
 })();
+
+// Display request or send div
+requestBtn.addEventListener("click", () => {
+  console.log("request div is shown");
+  requestDiv.classList.remove("hide");
+  sendDiv.classList.add("hide");
+});
+
+// Display send div
+sendBtn.addEventListener("click", () => {
+  sendDiv.classList.remove("hide");
+  requestDiv.classList.add("hide");
+});
