7e3baae6d3a676e179f9ab859c239f6ad1df78ad
damian.velazquez@uadec.edu.mx <damianvelazquez@uadec.edu.mx>
2025-10-25 06:59:27 -0600

    refactor auth endpoints

--- src/authRoutes/authRoutes.js
+++ src/authRoutes/authRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 // New API
 router.post("/signup", async (req, res) => {
   const { name, username, email, password } = req.body;
+  let user;
   console.log(name, email);
 
   // Check for valid email
@@ -16,13 +17,6 @@ router.post("/signup", async (req, res) => {
     });
   }
 
-  // generate password
-  const hashedPassword = User.generateSecurePassword(password);
-
-  // create instance
-  const user = new User(name, username, email, hashedPassword);
-  console.log(user);
-
   try {
     // Verify uniqueness
     const unique = User.unique(username, email);
@@ -30,35 +24,41 @@ router.post("/signup", async (req, res) => {
       return res.status(400).json({
         message: "Username or email already in use",
       });
+    } else {
+      // generate password
+      const hashedPassword = User.generateSecurePassword(password);
+
+      // create instance
+      user = new User(name, username, email, hashedPassword);
+      console.log(user);
+
+      // PRISMA Insert user (returns userId)
+      await user.insertUser();
+
+      // Confirm insert into DB
+      const userData = await user.getUser();
+
+      // Create JWT token
+      const token = user.createToken();
+
+      // Send response
+      res.status(201).json({
+        message: `User succesfully created`,
+        username: `${userData.username}`,
+        token: token,
+      });
+
+      // create the instance for the Account
+      const account = new Account(userData.username, userData.id);
+      console.log(account);
+
+      // Add account to accounts table
+      await account.initializeAccount();
+
+      // Set the registration bonus
+      account.setInitialBalance(accountData);
+      console.log(account);
     }
-
-    // PRISMA Insert user (returns userId)
-    await user.insertUser();
-
-    // Confirm insert into DB
-    const userData = await user.getUser();
-
-    // Create JWT token
-    const token = user.createToken();
-
-    // Send response
-    res.status(201).json({
-      message: `User succesfully created`,
-      username: `${userData.username}`,
-      token: token,
-    });
-
-    // create the instance for the Account
-    const account = new Account(userData.username, userData.id);
-    console.log(account);
-
-    // Add account to accounts table
-    await account.initializeAccount();
-
-    // Set the registration bonus
-    account.setInitialBalance(accountData);
-    console.log(account);
-
     //
   } catch (err) {
     console.log(err.message);
@@ -67,6 +67,7 @@ router.post("/signup", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
+  let user;
   console.log(username);
 
   // find user by username
@@ -77,15 +78,16 @@ router.post("/login", async (req, res) => {
     return res.status(401).json({
       message: "User does not exist",
     });
+  } else {
+    // Create the instance to access the methods
+    user = new User(
+      result.name,
+      result.username,
+      result.email,
+      result.password,
+      result.id
+    );
   }
-
-  const user = new User(
-    result.name,
-    result.username,
-    result.email,
-    result.password,
-    result.id
-  );
 
   try {
     // hash password
